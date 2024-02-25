a229d0e086af51ae2cb3989005e68f8e09d24d40
cristinedior <148567509+cristinedior@users.noreply.github.com>
2024-02-25 11:25:34 -0600

    removed package-lock.json and re-added to sync with package.json

--- config/connection.js
+++ config/connection.js
@@ -2,17 +2,14 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 let sequelize;
-
 if (process.env.JAWSDB_URL) {
-  // Use JAWSDB_URL for Heroku
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  // Fallback to localhost if JAWSDB_URL isn't set
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306,
-  });
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306,
+    });
 }
 
-module.exports = sequelize;module.exports = sequelize;
+module.exports = sequelize;
