f5a7780b20469653b885dc2c773550e7f8e24572
cristinedior <148567509+cristinedior@users.noreply.github.com>
2024-02-25 11:07:27 -0600

    cjones branch has different connection.js to access cloud based sequelize for Heroku app deployment

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,18 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  // Use JAWSDB_URL for Heroku
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  // Fallback to localhost if JAWSDB_URL isn't set
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306,
-  }
-);
+  });
+}
 
-module.exports = sequelize;
+module.exports = sequelize;module.exports = sequelize;
